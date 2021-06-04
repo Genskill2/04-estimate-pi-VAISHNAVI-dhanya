f8844681241e4efc3d21d62137b11e6c26c3cd8c
Vaishnavi-dhanya <vaishnavidhanya1999@gmail.com>
2021-06-04 13:04:23 +0530

    monte carlo

--- monte_carlo.c
+++ monte_carlo.c
@@ -3,8 +3,35 @@
 #include <stdio.h>
 #include <math.h>
 
-float mc_pi(int);
+float mc_pi(int n)
 
+{ /*float a = frandom();
+   printf("%f", a);*/
+  float pi;
+  int circle=0;
+  int sqr=0;
+  for( int i=1;i<=n;i++)
+     {
+       
+       long int q = random();
+       float x = (float)q/(float)RAND_MAX;
+       long int p = random();
+       float y = (float)p/(float)RAND_MAX;
+       /*x=frandom();
+       y=frandom();*/
+       if((x*x)+(y*y)<=1)
+        {
+         circle=circle+1;
+        }
+        else
+        {
+          sqr=sqr+1;
+        }
+      }
+    pi=(float)4*circle/(sqr+circle);
+    return pi;
+ } 
+    
 float frandom() {
   long int q = random();
   float ret = (float)q/(float)RAND_MAX;
